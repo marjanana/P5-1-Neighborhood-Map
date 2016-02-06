83f7a2f6782f2a41a1a9e64c08390641bf175be1
Ana Marjanovic <anamyers03@gmail.com>
2016-02-05 21:45:25 -0800

    responsive1

---
+++ js/app.js
@@ -0,0 +1,5 @@
+function appViewModel() {
+  var self = this;
+  var gymLocations = [];
+
+}
--- js/map.js
+++ js/map.js
@@ -3,7 +3,7 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat:40.811550, lng: -73.946477},
-    zoom: 13
+    zoom: 12
   });
 
 //resize and center map based on window size

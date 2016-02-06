4097d2da7fd261520f272562c8b027ae756953e5
Ana Marjanovic <anamyers03@gmail.com>
2016-02-05 21:21:46 -0800

    responsive design

--- js/map.js
+++ js/map.js
@@ -1,12 +1,18 @@
-//initiates the map
+//initiate map
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat:40.811550, lng: -73.946477},
     zoom: 13
   });
+
+//resize and center map based on window size
+  google.maps.event.addDomListener(window, 'load', initialize);
+  google.maps.event.addDomListener(window, "resize", function() {
+     var center = map.getCenter();
+     google.maps.event.trigger(map, "resize");
+     map.setCenter(center);
+  });
 }
-
-
 
 
